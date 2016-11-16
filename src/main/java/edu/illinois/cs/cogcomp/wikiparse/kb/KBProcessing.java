4e084c1272503671cc1ccf1fa8ebc1d593ac7bc4
Nitish Gupta <gnnitish@gmail.com>
2016-11-16 14:17:17 -0600

    1. Parses complete wikipedia to write all docs and links in separate folders. 2. KB, finds intersection of parsed wiki and typed pruned entities from freebase to make wid->mid maps, wid->WikiTitle map, finds all redirects that point to pages in our KB. 3. Writes wid.Wikitile file for entities in our KB, redirect.Wikititle (needed for crosswikis)

--- src/main/java/edu/illinois/cs/cogcomp/wikiparse/kb/KBProcessing.java
+++ src/main/java/edu/illinois/cs/cogcomp/wikiparse/kb/KBProcessing.java
@@ -11,16 +11,23 @@ import edu.illinois.cs.cogcomp.nlp.tokenizer.IllinoisTokenizer;
 import edu.illinois.cs.cogcomp.nlp.utility.TokenizerTextAnnotationBuilder;
 import edu.illinois.cs.cogcomp.wikiparse.jwpl.WikiDB;
 import edu.illinois.cs.cogcomp.wikiparse.util.io.FileUtils;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.Word;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.process.PTBTokenizer;
+import edu.stanford.nlp.process.TokenizerFactory;
+import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.TypesafeMap;
 import org.sweble.wikitext.engine.Compiler;
 import org.sweble.wikitext.lazy.parser.LazyRatsParser;
 import org.sweble.wikitext.lazy.utils.ParserShouldNotBeHereException;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -34,114 +41,185 @@ public class KBProcessing {
 	public static int min_sentences = 10;
 	public static int num_sent_per_mention = 4;
 
-	public static String getLinks(Page page) {
-		StringBuffer sbuf = new StringBuffer();
-		Set<Page> outlinks = page.getOutlinks();
-		for (Page p : outlinks) {
+	public static class TextLinks {
+		String pageId;
+		String text;
+		String links;
+		int num_sentences;
+		boolean wikifail;
+		boolean tafail;
+
+		public TextLinks (Page page) {
+			this.wikifail = false;
+			this.tafail = false;
+			this.text = "";
+			this.links = "";
+			this.num_sentences = 0;
+			this.pageId = Integer.toString(page.getPageId());
+			getText(page);
+			if (this.text != null && this.links != null && this.num_sentences != 0)
+				getLinks(page);
+		}
+
+		public void getText(Page page) {
+			StringBuffer sbuf = new StringBuffer();
+			int sentences_written = 0;
+			String pagePlainText = null;
 			try {
-				String title = p.getTitle().toString();
-				title = title.replaceAll("\\s", " ").trim();
-				sbuf.append(title + " ");
-			} catch (WikiTitleParsingException e) {
+				pagePlainText = page.getPlainText();
+			} catch (WikiApiException e) {
+				this.wikifail = true;
+				this.text = null;
+				this.num_sentences = 0;
+				this.links = null;
+				System.out.println("Wiki Parsing Doc Failed : " + page.getPageId());
+				return;
 			}
-		}
-		return sbuf.toString().trim();
-	}
-
-	public static String getText(Page page) {
-		StringBuffer sbuf = new StringBuffer();
-		int sentences_written = 0;
-
-		try {
-			String[] paras = page.getPlainText().split("\n");
-			for (String para : paras) {
-				TextAnnotation ta = tab.createTextAnnotation("", "", para);
-				if (ta.sentences().size() <= 1)
-					continue; // Most probably Section Headings.
-				for (Sentence sent : ta.sentences()) {
-					String text = sent.getTokenizedText().replaceAll("\\s", " ").trim();
-					if (!(text.split(" ").length < 4)) {
-						sbuf.append(text);
-						sbuf.append(" <eos_word> ");
-						sentences_written++;
-						if (sentences_written >= max_sentences)
-							break;
+			try {
+				// Different Paragraphs in Wiki Page
+				// Splitting to remove Section Headings
+				String[] paras = pagePlainText.split("\n");
+				for (String para : paras) {
+					TextAnnotation ta = tab.createTextAnnotation("", "", para);
+					// Most probably Section Headings.
+					if (ta.sentences().size() <= 1)
+						continue;
+					// If legitimate section para
+					for (Sentence sent : ta.sentences()) {
+						int num_tokens_in_sent = sent.getTokens().length;
+						if (!(num_tokens_in_sent < 4)) {
+							String text = sent.getTokenizedText().replaceAll("\\s", " ").trim();
+							sbuf.append(text);
+							sbuf.append(" "); sbuf.append("<eos_word>"); sbuf.append(" ");
+							sentences_written++;
+							if (sentences_written >= max_sentences)
+								break;
+						}
 					}
+					if (sentences_written >= max_sentences)
+						break;
 				}
-				if (sentences_written >= max_sentences)
-					break;
+			} catch (Exception e) {
+				this.tafail = true;
+				this.text = null;
+				this.num_sentences = 0;
+				this.links = null;
+				e.printStackTrace();
+				System.out.println("TA Doc Failed : " + page.getPageId());
+				return;
 			}
-			if (sentences_written < min_sentences)
-				return null;
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("\n\nDoc Failed : " + page.getPageId());
-			return null;
+
+			String text = sbuf.toString().trim();
+			if (text.equals("")) {
+				this.text = null;
+				this.num_sentences = 0;
+				this.links = null;
+			} else {
+				this.text = text;
+				this.num_sentences = sentences_written;
+			}
+	}
+
+		public void getLinks(Page page) {
+			StringBuffer sbuf = new StringBuffer();
+			Set<Page> outlinks = page.getOutlinks();
+			for (Page p : outlinks) {
+				try {
+					String title = p.getTitle().toString();
+					title = title.replaceAll("\\s", " ").trim();
+					sbuf.append(title);
+					sbuf.append("\t");
+				} catch (WikiTitleParsingException e) {
+				}
+			}
+			String links = sbuf.toString().trim();
+			if (links.equals(""))
+				links = "<unk_word>";
+
+			this.links = links;
 		}
 
-		tab.createTextAnnotation("", "", "asd");
-		return sbuf.toString().trim();
-
-
 	}
-
-	public static String getTextAndLinks(Page page) {
-		String text = getText(page);
-		if (text == null)
-			return null;
-		String links = getLinks(page);
-		StringBuffer sbf = new StringBuffer();
-		sbf.append(text);
-		sbf.append("\t");
-		sbf.append(links);
-		return sbf.toString();
-	}
-
 
 	static class ProcessPage implements Runnable {
-		String textandlinks;
-		String outputFilePath;
+		String text;
+		String links;
+		String outputDocsPath;
+		String outputLinksPath;
 
-		public ProcessPage(String textandlinks, String outputpath) {
-			this.textandlinks= textandlinks;
-			this.outputFilePath = outputpath;
+		public ProcessPage(String text, String links, String outputDocsPath, String outputLinksPath) {
+			this.text = text;
+			this.links = links;
+			this.outputDocsPath = outputDocsPath;
+			this.outputLinksPath = outputLinksPath;
 		}
 
 		public void run() {
-			FileUtils.writeStringToFile(outputFilePath, textandlinks);
+			FileUtils.writeStringToFile(outputDocsPath, this.text);
+			FileUtils.writeStringToFile(outputLinksPath, this.links);
 		}
 	}
 
-	public static void writeWikiDocs(String outputDir) throws WikiApiException {
+	public static void writeWikiDocs(String outputKBDir) throws Exception {
+		System.out.println("Writing WIkipedia Docs .. ");
+		String outputDocsDir = outputKBDir + "docs/";
+		String outputCoherenceDir = outputKBDir + "links/";
+		BufferedWriter bw_wikifail = new BufferedWriter(new FileWriter(outputKBDir + "wiki_failed"));
+		BufferedWriter bw_tafail = new BufferedWriter(new FileWriter(outputKBDir + "ta_failed"));
+
+		System.out.println("Writing docs to : " + outputDocsDir);
+		System.out.println("Writing links to : " + outputCoherenceDir);
+		System.out.println("Failed Wiki Docs to : " + outputKBDir + "wiki_failed");
+		System.out.println("Failed Annotation Docs to : " + outputKBDir + "ta_failed");
+
 		ExecutorService executor = Executors.newFixedThreadPool(45);
 		PageIterator iterator = new PageIterator(WikiDB.wiki, true, 100000);
 
-		int i = 0, totaldocs = 0;
+		int docs_processed = 0, docs_written = 0;
 		while (iterator.hasNext()) {
 			Page page = iterator.next();
-			totaldocs++;
-			if (KB.wids_FoundInWiki.contains(Integer.toString(page.getPageId()))) {
-				String outpath = outputDir + page.getPageId();
-				String textandlinks = getTextAndLinks(page);
-				if (textandlinks == null)
-					continue;
-				ProcessPage pp = new ProcessPage(textandlinks, outpath);
-				executor.submit(pp);
-				if (i % 10000 == 0)
-					System.out.print(i + " .. ");
-				i++;
+			try {
+				if (!page.isRedirect() && !page.isDisambiguation() && !page.isDiscussion() &&
+						!page.getTitle().getPlainTitle().startsWith("List of") &&
+						!page.getTitle().getPlainTitle().startsWith("Lists of")) {
+
+					docs_processed++;
+					String outputDocPath = outputDocsDir + Integer.toString(page.getPageId());
+					String outputLinksPath = outputCoherenceDir + Integer.toString(page.getPageId());
+					TextLinks textlinks = new TextLinks(page);
+					// Page Failed OR Empty
+					if (textlinks.text == null && textlinks.num_sentences == 0 && textlinks.links == null) {
+						if (textlinks.wikifail) {
+							bw_wikifail.write(page.getPageId());
+							bw_wikifail.write("\n");
+						} else if (textlinks.tafail) {
+							bw_tafail.write(page.getPageId());
+							bw_tafail.write("\n");
+						}
+					} else {
+						ProcessPage pp = new ProcessPage(textlinks.text, textlinks.links, outputDocPath, outputLinksPath);
+						executor.submit(pp);
+						docs_written++;
+					}
+					if (docs_processed % 100 == 0) {
+						System.out.print(docs_processed + " (" +
+										docs_written + ") ... ");
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-			if (i % 100000 == 0)
-				System.out.print("(" + i + ")" + " .. ");
 		}
 		System.out.println("\nDONEEE");
 		try {
-			executor.awaitTermination(1, TimeUnit.DAYS);
+			executor.awaitTermination(1, TimeUnit.HOURS);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		executor.shutdownNow();
 		System.out.println("DONEEEEEEEE");
+		bw_tafail.close();
+		bw_wikifail.close();
 		return;
 	}
 
@@ -229,15 +307,23 @@ public class KBProcessing {
 	public static void main(String[] args) throws Exception {
 		long startTime = System.currentTimeMillis();
 		//KB.widMidMap_FoundInWiki.keySet().size();
+
+		System.out.println("\n\n " + WikiDB.getPage("Eddie Vedder").getTitle().getWikiStyleTitle() + "\n\n");
+
+
+
+		//writeWikipediaDocuments();
+		writeWikiDocs("/save/ngupta19/wikipedia/wiki_kb/");
+//		writeMentions("/save/ngupta19/wikipedia/wiki_kb_10_15/docs_links/",
+//									"/save/ngupta19/wikipedia/wiki_kb_10_15/mentions_new/");
+
+
+
+
 		long estimatedTime = System.currentTimeMillis() - startTime;
 		double tt = ((double) (estimatedTime)) / (1000.0 * 60.0);
-
-
 		System.out.println("Total Time : " + tt + "  minutes");
-		//writeWikipediaDocuments();
-		//writeWikiDocs("/save/ngupta19/wikipedia/wiki_kb_10_15/docs_links/");
-		writeMentions("/save/ngupta19/wikipedia/wiki_kb_10_15/docs_links/",
-									"/save/ngupta19/wikipedia/wiki_kb_10_15/mentions_new/");
+
 		System.exit(1);
 	}
 }
---
+++ src/main/java/edu/illinois/cs/cogcomp/wikiparse/kb/WikiMentions.java
@@ -0,0 +1,170 @@
+package edu.illinois.cs.cogcomp.wikiparse.kb;
+
+import edu.illinois.cs.cogcomp.wikiparse.util.Constants;
+import edu.illinois.cs.cogcomp.wikiparse.util.io.FileUtils;
+import org.openrdf.query.algebra.In;
+
+import java.io.*;
+
+/**
+ * Created by nitishgupta on 11/13/16.
+ */
+public class WikiMentions {
+
+	public static final int max_mentions_perfile = 1000000;
+	public static final int max_mentions_per_entity = 3;
+
+	public static BufferedReader doc_vectors_reader = null;
+	public static BufferedReader links_vectors_reader = null;
+
+	static {
+		initializeDocLinksVectorsReaders();
+	}
+
+	public static void initializeDocLinksVectorsReaders() {
+		try {
+			doc_vectors_reader = new BufferedReader(new FileReader(Constants.wiki_doc_vectors_file));
+			links_vectors_reader = new BufferedReader(new FileReader(Constants.wiki_links_vectors_file));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	static class WIDSentences {
+		String[] sentences;
+		String wid;
+		String mid;
+		String wikiTitle;
+		int num_sentences;
+
+		public WIDSentences(String wid) {
+			this.wid = wid;
+			this.wikiTitle = KB.wid2WikiTitle.get(wid);
+			String doc_text = FileUtils.readFileToString(Constants.wiki_kb_docsDir + wid);
+			String[] sentences = doc_text.split("<eos_word>");
+			this.num_sentences = sentences.length;
+			this.sentences = new String[num_sentences];
+			for (int i=0; i<num_sentences; i++) {
+				String sent = sentences[i].trim() + " <eos_word>";
+				this.sentences[i] = sent;
+			}
+			this.mid = KB.wid2mid.get(wid);
+		}
+	}
+
+	static class DocLinksVectors {
+		String wid;
+		String[] doc_vectors_split;
+		String[] link_vectors_split;
+
+		// One line each from doc_vec and links_vec file.
+		// Should be running in sync and hence have vectors for the same entity. Still ASSERT
+		public DocLinksVectors(String doc_vecs_line, String links_vecs_line) {
+			String[] doc_vecs_split = doc_vecs_line.trim().split("\t");
+			String[] links_vecs_split = links_vecs_line.trim().split("\t");
+			assert(doc_vecs_split[0].equals(links_vecs_split[0]));
+			this.wid = doc_vecs_split[0];
+			this.doc_vectors_split = doc_vecs_split;
+			this.link_vectors_split = links_vecs_split;
+		}
+	}
+
+	public static BufferedWriter getNewMentionFileWriter (String outDir, String mention_file_root, int file_num) {
+		BufferedWriter bwr = null;
+		try {
+			String filepath = outDir + mention_file_root + Integer.toString(file_num);
+			bwr = new BufferedWriter(new FileWriter(filepath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return bwr;
+	}
+
+	public static void writeMentions() {
+		try {
+			long startTime = System.currentTimeMillis();
+			long rep_startTime = System.currentTimeMillis();
+
+			String outDir = Constants.wiki_kb_mentionsdir;
+			String mentions_file_rootname = "mentions.";
+			int file_num = 0;
+			int total_mentions_written = 0;
+			int mentions_written_infile = 0;
+			int docs_read = 0;
+			String doc_vecs_line = doc_vectors_reader.readLine();
+			String links_vecs_line = links_vectors_reader.readLine();
+			BufferedWriter bwr = getNewMentionFileWriter(outDir, mentions_file_rootname, file_num);
+			while (doc_vecs_line != null && links_vecs_line != null) {
+				DocLinksVectors doclinksvectors = new DocLinksVectors(doc_vecs_line, links_vecs_line);
+				String wid = doclinksvectors.wid;
+				assert (KB.wid2WikiTitle.containsKey(wid));
+				WIDSentences widsentences = new WIDSentences(wid);
+				int mentions_to_write = (widsentences.num_sentences > max_mentions_per_entity)
+								? max_mentions_per_entity : widsentences.num_sentences;
+
+				StringBuilder mentions_for_wid = new StringBuilder();
+				for (int sent_num = 0; sent_num < mentions_to_write; sent_num++) {
+					for (int vec_num = 1; vec_num <= 5; vec_num++) {
+						mentions_for_wid.append(widsentences.mid);
+						mentions_for_wid.append("\t");
+						mentions_for_wid.append(widsentences.wid);
+						mentions_for_wid.append("\t");
+						mentions_for_wid.append(widsentences.wikiTitle);
+						mentions_for_wid.append("\t");
+						mentions_for_wid.append(widsentences.sentences[sent_num]);
+						mentions_for_wid.append("\t");
+						mentions_for_wid.append(widsentences.wid);
+						mentions_for_wid.append("\t");
+						mentions_for_wid.append(doclinksvectors.doc_vectors_split[vec_num]);
+						mentions_for_wid.append("\t");
+						mentions_for_wid.append(doclinksvectors.link_vectors_split[vec_num]);
+						mentions_for_wid.append("\n");
+						mentions_written_infile++;
+						total_mentions_written++;
+					}
+				}
+
+				bwr.write(mentions_for_wid.toString());
+				if (mentions_written_infile >= max_mentions_perfile) {
+					bwr.close();
+					mentions_written_infile = 0;
+					file_num++;
+					bwr = getNewMentionFileWriter(outDir, mentions_file_rootname, file_num);
+				}
+
+				doc_vecs_line = doc_vectors_reader.readLine();
+				links_vecs_line = links_vectors_reader.readLine();
+				docs_read++;
+
+				if (docs_read % 10000 == 0) {
+					long estimatedTime = System.currentTimeMillis() - rep_startTime;
+					rep_startTime = System.currentTimeMillis();
+					double tt = ((double) (estimatedTime)) / (1000.0 * 60.0);
+					System.out.print(docs_read + " (" + total_mentions_written + ", " + tt + " mins) .. ");
+				}
+			}
+
+			bwr.close();
+			doc_vectors_reader.close();
+			links_vectors_reader.close();
+
+			long estimatedTime = System.currentTimeMillis() - startTime;
+			double tt = ((double) (estimatedTime)) / (1000.0 * 60.0);
+			System.out.println("\n" + docs_read + " (" + total_mentions_written + ", " + tt + " mins) .. ");
+
+		} catch (IOException e ) {
+			e.printStackTrace();
+		}
+
+
+
+	}
+
+
+
+
+	public static void main(String [] args) {
+		WikiMentions.writeMentions();
+
+	}
+}
