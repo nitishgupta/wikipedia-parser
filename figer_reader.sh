e85e83ec0f2645ae2159bce5d9f38f5878aff2aa
Nitish Gupta <gnnitish@gmail.com>
2017-02-17 23:36:48 -0600

    Scripts to run various class methods

--- figer_reader.sh
+++ figer_reader.sh
@@ -1,11 +1,7 @@
-#mvn clean
-#mvn dependency:copy-dependencies
 mvn compile
 
-DEFAULT_PACKAGE="edu.illinois.cs.cogcomp.wikiparse"
-#PACKAGE="be.tarsos.lsh"
-#MAINCLASS="CommandLineInterface"
-PACKAGE="datasets"
-MAINCLASS="WriteMentions"
+DEFAULT_PACKAGE="edu.washington.cs.figer"
+PACKAGE="data"
+MAINCLASS="MentionReader"
 CP="./:./target/classes/:./target/dependency/*:./config/:target/dependency/*"
 java -Xmx30g -cp $CP $DEFAULT_PACKAGE.$PACKAGE.$MAINCLASS                                                         
