c55532c0774bc9648baee5f1cf57f5ced5c032db
Nitish Gupta <gnnitish@gmail.com>
2017-03-03 13:32:45 -0600

    adding scripts again. Something funny happened

---
+++ scripts/agiga.sh
@@ -0,0 +1,11 @@
+#mvn clean
+#mvn dependency:copy-dependencies
+mvn compile
+
+DEFAULT_PACKAGE="edu.illinois.cs.cogcomp.wikiparse"
+#PACKAGE="be.tarsos.lsh"
+#MAINCLASS="CommandLineInterface"
+PACKAGE="agiga"
+MAINCLASS="ProcessAGIGA"
+CP="./:./target/classes/:./target/dependency/*:./config/:target/dependency/*"
+java -Xmx30g -cp $CP $DEFAULT_PACKAGE.$PACKAGE.$MAINCLASS                                                         
