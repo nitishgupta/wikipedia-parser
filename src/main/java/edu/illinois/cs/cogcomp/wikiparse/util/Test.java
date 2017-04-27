76e4dd26d8c17fe1694897d8f4410ed1283ddaca
Nitish Gupta <gnnitish@gmail.com>
2017-04-27 14:49:43 -0500

    Test contains sample code for Stanford CoreNLP

--- src/main/java/edu/illinois/cs/cogcomp/wikiparse/util/Test.java
+++ src/main/java/edu/illinois/cs/cogcomp/wikiparse/util/Test.java
@@ -1,10 +1,14 @@
 package edu.illinois.cs.cogcomp.wikiparse.util;
 
-import edu.illinois.cs.cogcomp.annotation.TextAnnotationBuilder;
-import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation;
-import edu.illinois.cs.cogcomp.nlp.tokenizer.IllinoisTokenizer;
-import edu.illinois.cs.cogcomp.nlp.tokenizer.Tokenizer;
-import edu.illinois.cs.cogcomp.nlp.utility.TokenizerTextAnnotationBuilder;
+
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+
+import java.util.List;
+import java.util.Properties;
 
 /**
  * Created by nitishgupta on 11/7/16.
@@ -12,48 +16,38 @@ import edu.illinois.cs.cogcomp.nlp.utility.TokenizerTextAnnotationBuilder;
 public class Test {
 
 	public static void main(String [] args) {
-//		String s = "Los Angeles ( \"The Angels\"),[14] officially the City of" +
-//		"Los Angeles and often known by its initials.\ndfa asdf asdfa sdfas well.";
-//
-//		Properties props = new Properties();
-//		props.setProperty("annotators", "tokenize, ssplit");
-//		props.setProperty("tokenize.options", "ptb3Escaping=false");
-//		props.setProperty("ssplit.isOneSentence", "false");
-//
-//
-//		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-//		Annotation document = new Annotation(s);
-//		System.out.println(pipeline.getProperties());
-//		pipeline.annotate(document);
-//
-//
-//		List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
-//
-//		for(CoreMap sentence: sentences) {
-//			// traversing the words in the current sentence
-//			// a CoreLabel is a CoreMap with additional token-specific methods
-//			for (CoreLabel token: sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-//				// this is the text of the token
-//				String word = token.get(CoreAnnotations.TextAnnotation.class);
-//				System.out.print(word + " ");
-//
-//			}
-//			System.out.println();
-//
-//		}
+		String s = "Bandar Seri Begawan 11-15 ( AFP ). A high - level American official announced today Wednesday in the wake of the meeting between American President Bill Clinton and his Russian counterpart Vladimir Putin that Putin 's proposals on reducing the Russian and American nuclear capability to under 1500 nuclear heads does not include \" many new elements . \"";
 
-		Tokenizer tkr = new IllinoisTokenizer();
-		String text = " "; // or "\n" or ""
-		Tokenizer.Tokenization tknzn = tkr.tokenizeTextSpan(text);
-		int[] sentEndOffsets = tknzn.getSentenceEndTokenIndexes();
-
-		TextAnnotationBuilder tab = new TokenizerTextAnnotationBuilder(new IllinoisTokenizer());
-		TextAnnotation ta = tab.createTextAnnotation(text);
+		Properties props = new Properties();
+		props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner");
+		props.setProperty("tokenize.options", "ptb3Escaping=false");
+		props.setProperty("tokenize.whitespace", "true");
+		props.setProperty("ssplit.isOneSentence", "true");
 
 
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+		Annotation document = new Annotation(s);
+		System.out.println(pipeline.getProperties());
+		pipeline.annotate(document);
 
 
+		List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
 
+		for (CoreMap sentence : sentences) {
+			System.out.println("NEW SENTECNE");
+			// traversing the words in the current sentence
+			// a CoreLabel is a CoreMap with additional token-specific methods
 
+			for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
+				// this is the text of the token
+				String word = token.get(CoreAnnotations.TextAnnotation.class);
+				String nertag = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
+				System.out.print(word + "_" + nertag + " ");
+				//System.out.print(word + " ");
+
+			}
+			System.out.println();
+
+		}
 	}
 }
