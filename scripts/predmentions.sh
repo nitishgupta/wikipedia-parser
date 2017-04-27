6f9113f071965e08fac79e92041fdcab04487274
Nitish Gupta <gnnitish@gmail.com>
2017-04-27 14:47:26 -0500

    scripts update

---
+++ scripts/predmentions.sh
@@ -0,0 +1,10 @@
+#mvn clean
+#mvn dependency:copy-dependencies
+mvn compile
+
+DEFAULT_PACKAGE="edu.illinois.cs.cogcomp.wikiparse"
+#MAINCLASS="CommandLineInterface"
+PACKAGE="PredMentions"
+MAINCLASS="PredMentions"
+CP="./:./target/classes/:./target/dependency/*:./config/:target/dependency/*"
+java -Xmx30g -cp $CP $DEFAULT_PACKAGE.$PACKAGE.$MAINCLASS                                                         
